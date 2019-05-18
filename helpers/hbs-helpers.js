dfe7ef55473b14bfb285a9d17097c442f25728b2
Massimiliano Marcon <me@marcon.me>
2019-05-18 10:59:22 +0200

    Minor tweak to temp comparison

--- helpers/hbs-helpers.js
+++ helpers/hbs-helpers.js
@@ -18,5 +18,7 @@ module.exports = {
     toMonth: (number) => MONTHS[number - 1],
     toOrdinal: (number) => `${number}${['st', 'nd', 'rd'][((number + 90) % 100 - 10) % 10 - 1] || 'th'}`,
     lt: (v1, v2) => v1 < v2,
-    gt: (v1, v2) => v1 > v2
+    gt: (v1, v2) => v1 > v2,
+    rlt: (v1, v2) => Math.round(v1) < Math.round(v2),
+    rgt: (v1, v2) => Math.round(v1) > Math.round(v2)
 };
